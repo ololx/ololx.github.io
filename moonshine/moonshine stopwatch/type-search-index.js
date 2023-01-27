58a2f1fb74cf637c694ba862d18b1dc7fc0e8392
a-kropotin <a.kropotin@pixonic.com>
2023-01-27 21:53:23 +0200

    chore(*) try to host javadocs

--- moonshine/moonshine stopwatch/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.github.ololx.moonshine.stopwatch","l":"SimpleStopwatch"},{"p":"io.github.ololx.moonshine.stopwatch","l":"Stopwatch"}];updateSearchResults();
